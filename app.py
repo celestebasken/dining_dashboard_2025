9dc2b33d91830e092d639098033a915ccf171f83
celestebasken <celeste.basken@gmail.com>
2025-07-26 18:50:09 -0700

    Add sidebar title to enable pages menu

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Institutional Food Dashboard", layout="wide")
 
+st.sidebar.title("Navigation")
+
 st.title("Institutional Food Purchasing Dashboard")
 
 st.markdown("""
